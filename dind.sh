852be3a6c916347455d39c55d1da8d5e68d87f41
A. Yasuda <apolo.yasuda@ge.com>
2020-01-22 22:18:06 -0800

    add repo push.

--- dind.sh
+++ dind.sh
@@ -1,5 +1,98 @@
 #!/bin/bash
 
+function brew_checkin (){
+
+    cd /brew
+
+    echo "checking brew installation"
+
+    git config user.name "EC.Bot" && git config user.email "EC.Bot@ge.com" && {
+	{
+	    git add . && {
+		{
+		    git commit -m "EC Agent ${EC_TAG} homebrew config check-in." && {
+			if [ $? -eq 0 ];
+			then 
+			    {
+				git push origin ${BRANCH} &&
+				    echo "Change has been pushed."
+			    } || {
+				echo "Push error"
+				exit 1
+			    }
+			else 
+			    echo "No update has been made."
+			fi;
+		    }
+		}
+	    }
+	} || {
+	    echo "Nothing added by git."
+	}
+    }
+
+    cd /build
+}
+
+function sdk_external_checkin (){
+    
+    #cp ${GOPATH}/src/${DHOME}/README.md /${DIST}/README_${ARTIFACT}.md
+    cd ./${DIST}
+
+    echo "updating external sdk.."
+    
+    git config user.name "EC.Bot" && git config user.email "EC.Bot@ge.com" && git config core.fileMode false && {
+	{
+	    git add . && {
+		{
+		    git commit -m "EC Agent ${EC_TAG} check-in." && {
+			if [ $? -eq 0 ];
+			then 
+			    {
+				git tag $EC_TAG
+				git push -f origin ${BRANCH} --tags &&
+				    echo "Change has been pushed."
+			    } || {
+				echo "Push error"
+				exit 1
+			    }
+			else 
+			    echo "No update has been made."
+			fi;
+		    }
+		    
+		}
+	    }
+	} || {
+	    echo "Nothing added by git."
+	}
+    }
+
+    cd /build
+}
+
+function agent_tagging(){
+    
+    cd ${GOPATH}/src/${DHOME}
+
+    echo "tag agent release.."
+    
+    git config user.name "EC.Bot" && git config user.email "EC.Bot@ge.com" && git config core.fileMode false && {
+	    {
+		git tag $EC_TAG
+		git push -f origin ${BRANCH} --tags &&
+		    echo "agent repo is tagged."
+	    } || {
+		echo "Push error"
+		exit 1
+	    }
+	    
+	}
+
+    cd /build
+}
+
+
 git clone --depth 1 https://${GITLAB_TKN}@${GITLAB_URL}/platform-agnostic/agent.git ${GOPATH}/src/${DHOME} --branch ${BRANCH}
 ls -al ${GOPATH}/src/${DHOME}/src/${LIBPKG}
 mkdir -p ${GOPATH}/src/${LIBPKG}
@@ -15,6 +108,11 @@ git clone --depth 1 https://github.com/Enterprise-connect/vln-plg.git ${GOPATH}/
 #git clone --depth 1 https://${GITLAB_TKN}@${GITLAB_URL}/predix/auth-api.git ./src/${APIPKG} --branch ${BRANCH}
 
 ls -la && pwd
+
+echo "clonning homebrew formula conf.." 
+git clone --depth 1 https://${GITPUBTKN}@github.com/Enterprise-connect/homebrew-core.git /brew --branch ${ARTIFACT}
+
+cp ./${ARTIFACT}.rb /brew/Formula/${ARTIFACT}.rb
 
 echo "clonning external sdk.."
 git clone --depth 1 --branch ${BRANCH} https://${GITPUBTKN}@github.com/Enterprise-connect/sdk.git /${DIST}
@@ -35,3 +133,9 @@ ls -la ${GOPATH}/pkg/linux_amd64/${LIBPKG}
 chmod -R 755 ./
 go version
 python2 -u build.py
+
+brew_checkin
+sdk_external_checkin
+agent_tagging
+
+echo sdk build completed.
