0ecca8c130f02084f18e0b073b353660c4b9e233
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-11-06 13:56:50 -0800

    Update build.py

--- build.py
+++ build.py
@@ -42,10 +42,10 @@ def test_cipher():
 def main():
 
     print "generate linux_amd64 artifacts with race; dns resolved by system"
-    os.system("CGO_ENABLED=1 GOOS=linux GODEBUG=netdns=cgo GOARCH=amd64 go build -tags netgo -v -o /{}_linux_sys {}/*.go".format(BINARY,DHOME))
+    os.system("go build -tags netgo -v -a -o /{}_linux_sys {}/*.go".format(BINARY,DHOME))
     
-    print "generate linux_amd64 artifacts with race; dns resolved by go"
-    os.system("CGO_ENABLED=1 GOOS=linux GODEBUG=netdns=go GOARCH=amd64 go build -tags netgo -v -o /{}_linux_var {}/*.go".format(BINARY,DHOME))
+    #print "generate linux_amd64 artifacts with race; dns resolved by go"
+    #os.system("CGO_ENABLED=1 GOOS=linux GODEBUG=netdns=go GOARCH=amd64 go build -tags netgo -v -o /{}_linux_var {}/*.go".format(BINARY,DHOME))
 
     #print "generate linux_adm64 secuity api binary"
     #os.system("CGO_ENABLED=0 GOOS=linux GODEBUG=netdns=cgo GOARCH=amd64 go build -tags netgo -a -v -o /{}_linux {}/*.go".format(APIBIN, APISRC))
